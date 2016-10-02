44acd70fb9fd45735e0c650d09c30abbe4a4bde7
Lapineige <lapineige@lavache.com>
2016-10-02 23:14:29 +0200

    Fixed a set of small mistakes

--- Static/readingtime.js
+++ Static/readingtime.js
@@ -1,40 +1,47 @@
+'use strict';
+
 var reading_time = {
 
+    flux_list: null,
+    flux: null,
+    textContent: null,
+    words_count: null,
+    read_time: null,
+    reading_time: null,
+
     init: function() {
-        flux_list = $("div[id^='flux']");
+        var flux_list = $("div[id^='flux']");
 
         for(var i = 0; i < flux_list.length; i++) {
             //console.log($("div[id^='flux']")[i], "Length (words): ", reading_time.flux_words_count($("div[id^='flux']")[i]))
 
-            flux = $("div[id^='flux']")[i]
+            reading_time.flux = $("div[id^='flux']")[i]
 
-            words_count = reading_time.flux_words_count($("div[id^='flux']")[i]) // count the words
-            reading_time = reading_time.calc_read_time(words_count, 300)
+            reading_time.words_count = reading_time.flux_words_count($("div[id^='flux']")[i]) // count the words
+            reading_time.reading_time = reading_time.calc_read_time(reading_time.words_count, 300)
 
             // add the reading time just before the title
-            $("#" + flux.id + " ul.horizontal-list li.item.title")[0].textContent = '[' + reading_time + 'min] ' + $("#" + flux.id + " ul.horizontal-list li.item.title")[0].textContent
+            $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title")[0].textContent = '[' + reading_time.reading_time + 'min] ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title")[0].textContent
         }
     },
 
-    flux_words_count : function(flux) {
+    flux_words_count: function flux_words_count(flux) {
 
-        htmlContent = flux.innerHTML, // get htmlContent
-
-        textContent = flux.textContent; // get textContent
+        reading_time.textContent = flux.textContent; // get textContent
 
         // split the text to count the words correctly (source: http://www.mediacollege.com/internet/javascript/text/count-words.html)
-        textContent = textContent.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
-        textContent = textContent.replace(/[ ]{2,}/gi," ");//2 or more space to 1
-        textContent = textContent.replace(/\n /,"\n"); // exclude newline with a start spacing
+        reading_time.textContent = reading_time.textContent.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
+        reading_time.textContent = reading_time.textContent.replace(/[ ]{2,}/gi," ");//2 or more space to 1
+        reading_time.textContent = reading_time.textContent.replace(/\n /,"\n"); // exclude newline with a start spacing
 
-        return textContent.split(' ').length; // returns the number of words
+        return reading_time.textContent.split(' ').length; // returns the number of words
     },
 
-    calc_read_time : function(wd_count, speed) {
-        reading_time = Math.round(wd_count/speed)
-        if (reading_time === 0) {reading_time = '<1'}
-        //console.log('Reading time: ', reading_time)
-        return reading_time
+    calc_read_time : function calc_read_time(wd_count, speed) {
+        reading_time.read_time = Math.round(wd_count/speed)
+        if (reading_time.read_time === 0) {reading_time.read_time = '<1'}
+        //console.log('Reading time: ', reading_time.read_time)
+        return reading_time.read_time
     },
 };
 
