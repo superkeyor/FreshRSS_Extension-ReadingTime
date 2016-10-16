23281c910daf7e18a4da4ef7641381cede4819e0
Lapineige <lapineige@lavache.com>
2016-10-16 14:20:55 +0200

    Fix multiple times on feed loading issue Title length no longer included

--- Static/readingtime.js
+++ Static/readingtime.js
@@ -18,10 +18,13 @@ var reading_time = {
             reading_time.flux = $("div[id^='flux']")[i]
 
             reading_time.words_count = reading_time.flux_words_count($("div[id^='flux']")[i]) // count the words
-            reading_time.reading_time = reading_time.calc_read_time(reading_time.words_count, 300)
+            reading_time.reading_time = reading_time.calc_read_time(reading_time.words_count, 300) // change this number (in words) to your prefered reading speed
 
             // add the reading time just after the feed name
-            $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent = reading_time.reading_time + 'm| ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent
+            if ( !($("#" + reading_time.flux.id + " ul.horizontal-list li.item.title")[0].textContent.search(reading_time.reading_time + 'm|')) ) {
+                $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent = reading_time.reading_time + 'm| ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent
+            }
+
         }
     },
 
@@ -34,7 +37,11 @@ var reading_time = {
         reading_time.textContent = reading_time.textContent.replace(/[ ]{2,}/gi," ");//2 or more space to 1
         reading_time.textContent = reading_time.textContent.replace(/\n /,"\n"); // exclude newline with a start spacing
 
-        return reading_time.textContent.split(' ').length; // returns the number of words
+        return reading_time.textContent.split(' ').length - //raw number of words
+        flux.childNodes[0].childNodes[2].textContent.split(' ').length - // excluding website length
+        flux.childNodes[0].childNodes[4].textContent.split(' ').length - // excluding date length
+        flux.childNodes[0].childNodes[6].textContent.split(' ').length;  // excluding title length
+        //Not 100% acurate as it doesn't check if there are shown at the bottom of the article
     },
 
     calc_read_time : function calc_read_time(wd_count, speed) {
@@ -55,53 +62,3 @@ function add_load_more_listener() {
 
 
 window.onload = add_load_more_listener
-
-'use strict';
-
-var reading_time = {
-
-    flux_list: null,
-    flux: null,
-    textContent: null,
-    words_count: null,
-    read_time: null,
-    reading_time: null,
-
-    init: function() {
-        var flux_list = $("div[id^='flux']");
-
-        for(var i = 0; i < flux_list.length; i++) {
-            //console.log($("div[id^='flux']")[i], "Length (words): ", reading_time.flux_words_count($("div[id^='flux']")[i]))
-
-            reading_time.flux = $("div[id^='flux']")[i]
-
-            reading_time.words_count = reading_time.flux_words_count($("div[id^='flux']")[i]) // count the words
-            reading_time.reading_time = reading_time.calc_read_time(reading_time.words_count, 300)
-
-            // add the reading time just after the feed name
-            $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent = reading_time.reading_time + 'm| ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent
-        }
-    },
-
-    flux_words_count: function flux_words_count(flux) {
-
-        reading_time.textContent = flux.textContent; // get textContent
-
-        // split the text to count the words correctly (source: http://www.mediacollege.com/internet/javascript/text/count-words.html)
-        reading_time.textContent = reading_time.textContent.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
-        reading_time.textContent = reading_time.textContent.replace(/[ ]{2,}/gi," ");//2 or more space to 1
-        reading_time.textContent = reading_time.textContent.replace(/\n /,"\n"); // exclude newline with a start spacing
-
-        return reading_time.textContent.split(' ').length; // returns the number of words
-    },
-
-    calc_read_time : function calc_read_time(wd_count, speed) {
-        reading_time.read_time = Math.round(wd_count/speed)
-        if (reading_time.read_time === 0) {reading_time.read_time = '<1'}
-        //console.log('Reading time: ', reading_time.read_time)
-        return reading_time.read_time
-    },
-};
-
-
-window.onload = reading_time.init;
